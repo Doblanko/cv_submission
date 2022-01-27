240a7659e473877984e8df3e9c9974609b913c8f
Tyler  Doblanko <tylerdoblanko@gmail.com>
2022-01-26 21:59:50 -0700

    Basic Setup of App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      name: '',
+    });
+  };
+
+  render() {
+    // destructuring
+    const { name } = this.state;
+
+    return (
+      <div id="App">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="nameInput">
+            Name
+            <input
+              id="nameInput"
+              type="text"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Submit General Info</button>
+        </form>
+      </div>
+
+    );
+  }
 }
 
 export default App;
